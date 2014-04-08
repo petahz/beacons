65654b03b9b476adc3768bf1cc612801835149b9
lcavezza <laracavezza@gmail.com>
2014-04-08 15:40:58 -0400

    Gradient

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Beacons::Application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  get '/discover', to: 'static_pages#discover', as: 'discover'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
