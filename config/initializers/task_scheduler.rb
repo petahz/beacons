a94ef17d5c154b378741f7691fc2b09ec46ffbe6
Peter Tuan <xpetahzx@gmail.com>
2014-04-30 11:15:51 -0400

    rufus

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -4,6 +4,6 @@ scheduler = Rufus::Scheduler.start_new
 scheduler.every '10m' do
    require "net/http"
    require "uri"
-   url = 'http://www.beaconheroes.com'
+   url = 'http://codeglot.com'
    Net::HTTP.get_response(URI.parse(url))
 end
